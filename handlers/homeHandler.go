8225a5714507c89bdf99a5b30a7959a3ffc89444
Hamza Carew <hamzacarew@gmail.com>
2020-06-18 15:47:02 +0100

    Implemented pdf upload for home handler

--- handlers/homeHandler.go
+++ handlers/homeHandler.go
@@ -2,16 +2,27 @@ package handlers
 
 import (
 	"net/http"
+	"io"
+	"os"
+	"log"
 )
 
 // HomeHandler -
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	b := []byte("On the hello world.")
-	w.Write(b)
+	// Send PDF 
+	file, err := os.Open("./HamzaCV.pdf")
+	if err != nil { 
+		log.Println(err) 
+		return
+	}
+	defer file.Close()
 
-	
-}
+	// Set header for PDF
+	w.Header().Set("Content-type", "application/pdf")
 
-func servePDF () {
-	
+	// Stream bytes from file to response writer
+	w.WriteHeader(http.StatusOK)
+	if _, err := io.Copy(w, file); err != nil {
+		log.Println(err)
+	}
 }
