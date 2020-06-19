d21c69d41dc31503988aac99653c5df49f83b7d9
Hamza Carew <hamzacarew@gmail.com>
2020-06-19 19:44:19 +0100

    Added email functionality

---
+++ emails/emails.go
@@ -0,0 +1,35 @@
+package emails
+
+import (
+	"fmt"
+	"net/smtp"
+	"log"
+)
+
+// Email is placeholder for 
+type Email struct {
+	SMTPServerAddr 	string
+	Recipient 		string
+	Sender 			string
+	Body 			string
+}
+
+func (e *Email)	sendEmail() {
+	conn, err := smtp.Dial(e.SMTPServerAddr)
+	if err != nil { log.Println(err) }
+
+	// Set sender and recipient
+	if err := conn.Mail(e.Sender); err != nil { log.Println(err) }
+	if err := conn.Rcpt(e.Recipient); err != nil { log.Println(err) }
+
+	// Define closer to attach body to
+	w, err := conn.Data()
+	if err != nil { log.Println(err) }
+
+	// Stream data to email writer
+	fmt.Fprintf(w, e.Body)
+
+	// Close reader && connection
+	if err := w.Close(); err != nil { log.Println(err) }
+	if	err := conn.Close(); err != nil { log.Println(err)}
+}
