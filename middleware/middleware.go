7252e067f9dff50f633b08bb6e77c95e6d21b162
Hamza Carew <hamzacarew@gmail.com>
2020-06-20 12:36:04 +0100

    Logging middleware fixed

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -9,19 +9,26 @@ import (
 	"time"
 )
 
+var (
+	file *os.File
+)
+
+func init() {
+	f, err := os.Create("requestLogs.txt")
+	if err != nil { log.Println(err) }
+	file=f
+}
+
 // LogRequests tracks all the requests that hit an endpoint
 func LogRequests(next func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		file, err := os.Create("requestLogs.txt")
-		if err != nil { log.Println(err) }
-
 		host, _, _ := net.SplitHostPort(r.RemoteAddr)
 		curTime := time.Now()
 		t := fmt.Sprintf("%d:%d:%d", curTime.Hour(), curTime.Minute(), curTime.Second())
-
-		logger := log.New(file, t, 0)
-		logger.Printf(" - %s - [ %s ] - %s \n", r.Method, r.URL, host)
-
+		
+		output := fmt.Sprintf("%s - %s - [ %s ] - %s \n", t, r.Method, r.URL, host)
+		file.WriteString(output)
+		
 		// Execute handler
 		next(w, r)
 	}
